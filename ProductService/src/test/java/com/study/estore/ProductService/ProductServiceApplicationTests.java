97678ee7ae935f92bae5ef9e2c1de49f7bd7b254
Artem Petrenko <artem.petrenko@telekom.com>
2025-02-19 19:51:08 +0100

    productService init

---
+++ ProductService/src/test/java/com/study/estore/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.estore.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
