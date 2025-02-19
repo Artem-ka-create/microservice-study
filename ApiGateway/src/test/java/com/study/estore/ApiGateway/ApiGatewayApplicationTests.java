286314ac0201317e5e2bc4097bca741e414265ff
Artem Petrenko <artem.petrenko@telekom.com>
2025-02-19 20:48:32 +0100

    feat: init api gateway

---
+++ ApiGateway/src/test/java/com/study/estore/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.estore.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
