aeb42ac2f59de048bf36ea47083ee8d571d242aa
Artem Petrenko <artem.petrenko@telekom.com>
2025-02-19 20:42:30 +0100

    feat: eureka server configuration

--- ProductService/src/main/java/com/study/estore/ProductServiceApplication.java
+++ ProductService/src/main/java/com/study/estore/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package com.study.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
