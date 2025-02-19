99c2b2589b82b2564470084e4f05d805ac4ded04
Artem Petrenko <artem.petrenko@telekom.com>
2025-02-19 20:17:01 +0100

    feat: product logic fill, additional config files

--- ProductService/src/main/java/com/study/estore/ProductServiceApplication.java
+++ ProductService/src/main/java/com/study/estore/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.study.estore.ProductService;
+package com.study.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ProductService/src/test/java/com/study/estore/controller/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/study/estore/controller/ProductServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.estore.ProductService;
+package com.study.estore.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
